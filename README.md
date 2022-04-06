1f996d9a80a3cad73c8c8bc0ca978cecf81f3a6c
capralkein <capralkein@yandex.ru>
2022-04-06 06:33:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DogAnimation
+
+Test task
---
+++ Source/DogAnimation/DogAnimationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DogAnimationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOGANIMATION_API ADogAnimationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
